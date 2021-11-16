98bba6b5397568653a9403423074ced2285e46fa
Matt Leezer <84049292+mrleezer0@users.noreply.github.com>
2021-11-16 17:47:31 -0500

    video section api

--- youtubeapi.js
+++ youtubeapi.js
@@ -1,42 +1,11 @@
-var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'M7lc1UVf-VE',
-          playerVars: {
-            'playsinline': 1
-          },
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
+const searchTerms = ["air%20guitar", "airguitar", "milli$20vanilli", "snow%20informer%20remix", "guitar%20broom"];
+const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
+const YOUTUBE_API_KEY = "AIzaSyDcgG6zJ3aziQ6GDGV7AHfeyRhu1US9J5w";
+const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${YOUTUBE_API_KEY}`;
+// console.log(url);
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    //console.log(data.items[0].id.videoId);
+    document.querySelector(".youtubeVideo").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+});
