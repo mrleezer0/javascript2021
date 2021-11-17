b618f83ab3d279300d47de5877d69c5082937203
Matt Leezer <84049292+mrleezer0@users.noreply.github.com>
2021-11-16 21:15:14 -0500

    New api key

--- youtubeapi.js
+++ youtubeapi.js
@@ -1,11 +1,9 @@
-const searchTerms = ["air%20guitar", "airguitar", "milli$20vanilli", "snow%20informer%20remix", "guitar%20broom"];
+const searchTerms = ["air%20guitar", "airguitar", "milli$20vanilli", "snow%20informer%20remix", "guitar%20broom", "Gary%20Bertsch%20Air%20Guitar"];
 const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
-const YOUTUBE_API_KEY = "AIzaSyDcgG6zJ3aziQ6GDGV7AHfeyRhu1US9J5w";
+const YOUTUBE_API_KEY = "AIzaSyDsSVaoEwUXQYSCM53QX7u1rta3A3WKG1E";
 const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${YOUTUBE_API_KEY}`;
-// console.log(url);
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    //console.log(data.items[0].id.videoId);
     document.querySelector(".youtubeVideo").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
 });
