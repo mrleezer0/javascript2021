373b6902396a84e969c1072ea2cd4e3005391ffd
Matt Leezer <84049292+mrleezer0@users.noreply.github.com>
2021-11-18 20:09:28 -0500

    final update

--- email.js
+++ email.js
@@ -3,13 +3,13 @@ function ValidateEmail(inputText)
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 if(inputText.value.match(mailformat))
 {
-alert("Valid email address!");
+alert("Sweet Jesus it's valid");
 document.form1.text1.focus();
 return true;
 }
 else
 {
-alert("You have entered an invalid email address!");
+alert("Come on Bucko give me a valid address");
 document.form1.text1.focus();
 return false;
 }
